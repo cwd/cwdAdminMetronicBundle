8de5795ac4de9a0ee0b15e7201ff49fd4e0744a5
Ludwig Ruderstaller <lr@cwd.at>
2015-02-16 00:47:13 +0100

    email template

--- Resources/public/custom.js
+++ Resources/public/custom.js
@@ -8,3 +8,13 @@ var handleColorPicker = function () {
     $('.colorpicker-rgba').colorpicker();
 }
 handleColorPicker();
+
+
+$(document).on('click', 'a.modal-open', function(e){
+    e.preventDefault();
+
+    var $modal = $('#helper-modal');
+    $modal.load($(this).attr('href'), '', function(){
+        $modal.modal();
+    });
+});
